2ce6460adbccda3ff8470bdcd0f6a8dbaee6e0ca
Michael Nairn <michael.nairn@feedhenry.com>
2014-04-11 15:05:44 +0100

    Updated to add simple input and make act call.

---
+++ www/hello.js
@@ -0,0 +1,19 @@
+$fh.ready(function() {
+
+  document.getElementById('say_hello').onclick = function() {
+    $fh.act(
+        {
+          act:'hello',
+          req: {
+            hello: document.getElementById('hello_to').value
+          }
+        },
+        function(res) {
+          document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+        },
+        function(code,errorprops,params) {
+          alert('An error occured: ' + code + ' : ' + errorprops);
+        }
+    );
+  };
+});
