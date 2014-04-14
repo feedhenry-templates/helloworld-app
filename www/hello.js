c37f695b04090388d473191c384289c8702d25aa
Michael Nairn <michael.nairn@feedhenry.com>
2014-04-14 10:49:28 +0100

    Remove $fh.ready Move script tags to end of body

--- www/hello.js
+++ www/hello.js
@@ -1,19 +1,16 @@
-$fh.ready(function() {
-
-  document.getElementById('say_hello').onclick = function() {
-    $fh.act(
-        {
-          act:'hello',
-          req: {
-            hello: document.getElementById('hello_to').value
-          }
-        },
-        function(res) {
-          document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
-        },
-        function(code,errorprops,params) {
-          alert('An error occured: ' + code + ' : ' + errorprops);
+document.getElementById('say_hello').onclick = function () {
+  $fh.act(
+      {
+        act: 'hello',
+        req: {
+          hello: document.getElementById('hello_to').value
         }
-    );
-  };
-});
+      },
+      function (res) {
+        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+      },
+      function (code, errorprops, params) {
+        alert('An error occured: ' + code + ' : ' + errorprops);
+      }
+  );
+};
